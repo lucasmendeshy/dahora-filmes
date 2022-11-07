e4b91e5b0d76c5a863d6707d270f2b61f33b3602
Lucas Mendes <lucasaraujo12345@outlook.com.br>
2022-11-07 11:57:30 -0300

    Adicionando icones

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { SafeAreaView, Text, View, Image, Pressable } from "react-native";
 import { useFonts } from "expo-font";
 import estilos from "./estilos";
 import logo from "./assets/imagens/logo.png";
+import { Ionicons } from "@expo/vector-icons";
 
 const App = () => {
   const [fonteCarregada] = useFonts({
@@ -19,21 +20,29 @@ const App = () => {
 
       <View style={estilos.viewBotoes}>
         <Pressable style={estilos.botaoInicial}>
-          <Text style={estilos.textoBotao}>Buscar Filmes</Text>
+          <Text style={estilos.textoBotao}>
+            <Ionicons name="search" size={16} color="#cccc" /> Buscar Filmes
+          </Text>
         </Pressable>
 
         <Pressable style={estilos.botaoInicial}>
-          <Text style={estilos.textoBotao}>Favoritos</Text>
+          <Text style={estilos.textoBotao}>
+            <Ionicons name="star" size={16} color="gold" /> Favoritos
+          </Text>
         </Pressable>
       </View>
 
       <View style={estilos.viewRodape}>
         <Pressable style={estilos.botaoRodape}>
-          <Text style={estilos.textoBotao}>Privacidade</Text>
+          <Text style={estilos.textoBotao}>
+            <Ionicons name="lock-closed" size={16} color="#cccc" /> Privacidade
+          </Text>
         </Pressable>
 
         <Pressable style={estilos.botaoRodape}>
-          <Text style={estilos.textoBotao}>Sobre</Text>
+          <Text style={estilos.textoBotao}>
+            <Ionicons name="information-circle" size={16} color="#cccc" /> Sobre
+          </Text>
         </Pressable>
       </View>
     </SafeAreaView>
