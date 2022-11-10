2f7f0b9f9a3a3d1c57933977d77282efe9521e91
Lucas Mendes <lucasaraujo12345@outlook.com.br>
2022-11-10 10:49:49 -0300

    axios

---
+++ src/screens/services/Api.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const Api = () => {
+  return (
+    <View>
+      <Text>Api</Text>
+    </View>
+  );
+};
+
+export default Api;
+
+const styles = StyleSheet.create({});
