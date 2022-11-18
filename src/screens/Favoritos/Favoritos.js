2cec56cc53f1241a6da7f469402fc62dd24a692c
Lucas Mendes <lucasaraujo12345@outlook.com.br>
2022-11-18 10:28:25 -0300

    programação excluirUmFavorito

--- src/screens/Favoritos/Favoritos.js
+++ src/screens/Favoritos/Favoritos.js
@@ -1,5 +1,12 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { Alert, Pressable, SafeAreaView, Text, View } from "react-native";
+import {
+  Alert,
+  Pressable,
+  SafeAreaView,
+  ScrollView,
+  Text,
+  View,
+} from "react-native";
 import React, { useEffect, useState } from "react";
 import { Ionicons } from "@expo/vector-icons";
 import estilos from "./FavoritosEstilos";
@@ -10,10 +17,15 @@ const Favoritos = () => {
   useEffect(() => {
     async function carregarFavoritos() {
       try {
+        /* Acessar o storage de favoritos e tentar carregar os dados existentes */
         const dados = await AsyncStorage.getItem("@Favoritos");
+
+        /* Havendo dados, transformamos eles em array de objetos */
         const filmes = JSON.parse(dados);
+
+        /* Se realmente tem dados (ou seja, não é null), atualizamos o componente */
         if (dados != null) {
-          setListaFavoritos(filmes);
+          setListaFavoritos(filmes); /* State de dados do componente  */
         }
         console.log(dados);
       } catch (error) {
@@ -24,31 +36,70 @@ const Favoritos = () => {
   }, []);
 
   const excluirFavoritos = async () => {
+    /* Usamos removeItem para apagar somente os dados do @favoritos do nosso App */
     await AsyncStorage.removeItem("@Favoritos");
     setListaFavoritos([]);
     Alert.alert("Favoritos", "Favoritos excluídos!");
   };
 
+  const excluirUmFavorito = async (indice) => {
+    /* Alert.alert(`Excluir filme no indice: ${indice}`); */
+
+    /* Etapas para exclusão do filme escolhido */
+
+    // 1) conhecendo o índice, remover o elemento (filme do array listaFavoritos)
+    /* Splice -> indicamos o índice de referência (na prática, o índice do filme que queremos remover e, a partir deste índice, a quantidade de elementos que queremos remover. Como aqui queremos apagar somente o próprio filme escolhido, passamos 1) */
+    listaFavoritos.splice(indice, 1);
+
+    // 2) Atualizar o storage com a lista atualizada (ou seja, sem o filme)
+    /* Obs.: é necessário transformar em string antes de gravar no Storage */
+    await AsyncStorage.setItem("@Favoritos", JSON.stringify(listaFavoritos));
+
+    // 3) Recarregamento do storage a nova lista de favoritos
+    /* Obs.: é necessário transformar em array/objetos antes de manipular na aplicação */
+    const listaDeFilmes = JSON.parse(await AsyncStorage.getItem("@Favoritos"));
+
+    // 4) Atualizar o state para um novo render na tela com a lista de favoritos
+    setListaFavoritos(listaDeFilmes);
+  };
+
   return (
     <SafeAreaView style={estilos.safeContainer}>
       <View style={estilos.container}>
-        <Text>Quantidade: {listaFavoritos.length}</Text>
-        <Pressable style={estilos.botao} onPress={excluirFavoritos}>
-          <Text style={estilos.textoBotao}>Excluir favoritos</Text>
-        </Pressable>
+        <View style={estilos.cabecalho}>
+          <Text>Quantidade: {listaFavoritos.length}</Text>
+          <Pressable
+            style={estilos.botaoExcluirTudo}
+            onPress={excluirFavoritos}
+          >
+            <Text style={estilos.textoExcluirTudo}>
+              <Ionicons name="trash-outline" size={16} /> Excluir favoritos
+            </Text>
+          </Pressable>
+        </View>
 
-        {listaFavoritos.map((filmeFavorito) => {
-          return (
-            <>
-              <Pressable key={filmeFavorito.id} style={estilos.itemFilme}>
-                <Text>{filmeFavorito.title}</Text>
-                <Pressable style={estilos.botaoExcluir}>
-                  <Ionicons name="trash" size={24} color="white" />
+        {/*  <Pressable style={estilos.botao} onPress={excluirFavoritos}>
+          <Text style={estilos.textoBotao}>Excluir favoritos</Text>
+        </Pressable> */}
+        <ScrollView showsVerticalScrollIndicator={false}>
+          {listaFavoritos.map((filmeFavorito, indice) => {
+            return (
+              <>
+                <Pressable key={filmeFavorito.id} style={estilos.itemFilme}>
+                  <Text style={estilos.titulo}>{filmeFavorito.title}</Text>
+                  <Pressable
+                    style={estilos.botaoExcluir}
+                    //onPress={excluirUmFavorito}
+                    //onPress={() => excluirUmFavorito(indice)} // Sintaxe 1
+                    onPress={excluirUmFavorito.bind(this, indice)}
+                  >
+                    <Ionicons name="trash" size={16} color="white" />
+                  </Pressable>
                 </Pressable>
-              </Pressable>
-            </>
-          );
-        })}
+              </>
+            );
+          })}
+        </ScrollView>
       </View>
     </SafeAreaView>
   );
--- src/screens/Favoritos/FavoritosEstilos.js
+++ src/screens/Favoritos/FavoritosEstilos.js
@@ -4,31 +4,51 @@ const estilos = StyleSheet.create({
   container: {
     padding: 8,
     flex: 1,
+    backgroundColor: "white",
   },
-  botao: {
+  /*  botao: {
     padding: 12,
     borderRadius: 5,
     backgroundColor: "#5451a6",
-  },
-  textoBotao: {
+  }, */
+  /* textoBotao: {
     color: "white",
     fontSize: 16,
     textTransform: "capitalize",
     textAlign: "center",
-  },
+  }, */
   itemFilme: {
     padding: 8,
     flexDirection: "row",
     justifyContent: "space-between",
-    backgroundColor: "#ccc",
+    backgroundColor: "#f7f7f7",
     marginVertical: 8,
     borderRadius: 4,
     alignItems: "center",
   },
   botaoExcluir: {
     backgroundColor: "red",
-    padding: 12,
+    padding: 8,
     borderRadius: 4,
+  },
+  cabecalho: {
+    marginVertical: 8,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  botaoExcluirTudo: {
+    borderWidth: 1,
+    borderColor: "red",
+    padding: 8,
+    borderRadius: 4,
+  },
+  textoExcluirTudo: {
+    color: "red",
+  },
+  titulo: {
+    flex: 1,
+    fontSize: 14,
   },
 });
 export default estilos;
